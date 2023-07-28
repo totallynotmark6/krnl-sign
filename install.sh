9002dc289d5f42a04b5877744d275cc1ab8fb0ff
Mark Smith <totallynotmark6@gmail.com>
2023-07-28 12:20:47 -0500

    underp updater

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 sudo apt install -y python3-pip git
 git clone https://github.com/totallynotmark6/krnl-sign.git
 cd krnl-sign
+git config pull.rebase false
 sudo ./rgbmatrixinstall.sh
 sudo -H python3 -m pip install -e ~/krnl-sign
 sudo cp -v ./bin/krnl-sign.service /lib/systemd/system/
