a185bd2f0dd5ba9272531250ceb38540d27e1d74
Mark Smith <totallynotmark6@gmail.com>
2023-07-28 08:01:04 -0500

    convert to setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='krnl_sign',
+    version='0.0.1',    
+    description='funny sign go brrrrrrrr',
+    url='https://github.com/totallynotmark6/krnl-sign',
+    author='Mark Smith',
+    author_email='totallynotmark6@gmail.com',
+    license='none',
+    packages=['krnl_sign'],
+    install_requires=['requests',
+                      'arrow',                     
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+    ],
+)
